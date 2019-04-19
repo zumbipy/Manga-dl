b390c3f6f55e52089c6c2908f5561c3b070862bf
ZumbiPy <zumbipy@gmail.com>
2019-04-19 20:34:05 -0300

    Criação da função siteAceito()

--- dmanga.py
+++ dmanga.py
@@ -4,17 +4,23 @@ from downloadmanga import DownloadManga
 from unionmangas import UnioMangas
 
 
+def siteAceito(url, dic_url):
+    for url_aceitos in dic_url.values():
+        if url.startswith(url_aceitos):
+            return True
+    else:
+        return False
+
+dic_url = {
+    'Union Mangas':'https://unionmangas.top/'
+}
+
 if __name__ == "__main__":
-
-    while True:
-        url = input("Digite a URL(Link) do capitulo do manga: ")
-        if 'https://unionmangas.top/' in url:
-            break
-        else:
-            print("URL invalida. ")
-            exit()
-
-    baixaConteudo = DownloadManga(UnioMangas().get(url))
-    baixaConteudo.criarPastaTitulo()
-    baixaConteudo.criarPastaCapitulo()
-    baixaConteudo.downConteudo()
+    url = input("Digite o Link do site do manga: ")
+    if siteAceito(url, dic_url):
+        baixaConteudo = DownloadManga(UnioMangas().get(url))
+        baixaConteudo.criarPastaTitulo()
+        baixaConteudo.criarPastaCapitulo()
+        baixaConteudo.downConteudo()
+    else:
+        print(f"Site {url} não tem suporte.")
