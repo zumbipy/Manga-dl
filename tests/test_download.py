3fb3bcb03a7ccfdea43b7fd10f843e4611298a3c
ZumbiPy <zumbipy@gmail.com>
2020-01-31 15:40:59 -0300

    tests create

---
+++ tests/test_download.py
@@ -0,0 +1,5 @@
+from source.download import Download
+
+def test_downlad_image():
+    down = Download()
+    assert 1 == 1
---
+++ tests/test_manage.py
@@ -0,0 +1,4 @@
+from source.manage import Manage
+
+def test_manage_create_folder():
+    assert True
