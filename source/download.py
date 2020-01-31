3af16def9df76fd7f73fbb2bd0fa6a1d592d364c
ZumbiPy <zumbipy@gmail.com>
2020-01-30 21:39:54 -0300

    process bar

--- source/download.py
+++ source/download.py
@@ -1,6 +1,8 @@
 import os.path
 import requests
 
+from tqdm import tqdm
+
 
 class Download:
     """ class responsible for donwload as images"""
@@ -11,9 +13,13 @@ class Download:
         *_, image_name = url.split("/")
         image = requests.get(url, stream=True)
 
+        file_size = int(image.headers.get('content-length'))
+        chunk_size = 1024  # 1 MB
+        num_bars = int(file_size / chunk_size)
+
         if image.status_code == 200:
             with open(os.path.join(path, image_name), "wb") as f:
-                for chuck in image.iter_content(1024):
+                for chuck in tqdm(image.iter_content(chunk_size=chunk_size), total=num_bars, unit='KB', desc=f'        Download da Imagem {image_name}'):
                     f.write(chuck)
         else:
             return f"Erro Download {url}"
