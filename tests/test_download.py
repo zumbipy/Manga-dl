23411a81511e5e8275e811719a07f8dc1cc9d395
ZumbiPy <zumbipy@gmail.com>
2020-02-01 18:59:03 -0300

    test download file image

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,5 +1,9 @@
+import os.path
 from source.download import Download
 
+
 def test_downlad_image():
-    down = Download()
-    assert 1 == 1
+    Download().image('https://uploads.toptal.io/blog/image/252/toptal-blog-image-1389090346415.png', '/home/zumbipy/Projetos/Manga-dl/tests')
+    assert 'toptal-blog-image-1389090346415.png' in os.listdir('/home/zumbipy/Projetos/Manga-dl/tests')
+    os.remove('/home/zumbipy/Projetos/Manga-dl/tests/toptal-blog-image-1389090346415.png')
+    
